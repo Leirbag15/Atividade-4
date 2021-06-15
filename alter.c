bae90309cf8115c6ae16164b54f6129711b6457e
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-15 01:21:21 -0300

    Create alter.c

---
+++ alter.c
@@ -0,0 +1,15 @@
+int s,s1; // tamanhos dos vetores
+// 0=False- 1=True
+void alterna (int x){
+    int n[2x];
+    for (int i=0; i<=2x; i++){
+        n[i]=i;
+        i++;
+        }
+    for (int i=1; i<=2x; i++){
+        n[i]=(i)*(-1);
+        i++;
+        }
+   
+    
+}
