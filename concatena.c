61ed62331fc84c598404ee4400d468141bc1c80e
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-15 01:13:11 -0300

    Create concatena.c

---
+++ concatena.c
@@ -0,0 +1,12 @@
+int s,s1; // tamanhos dos vetores
+// 0=False- 1=True
+void concatena (int x, int v[], int v2[]){
+    int n[s+s1];
+    for (int i=0; i<s; i++){
+        n[i]=v[i];
+        }
+    for (int i=0; i<s1; i++){
+        n[s+i]=v2[s1];
+    }
+    
+}
