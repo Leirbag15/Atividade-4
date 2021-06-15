cd95f575f8841e3454d057fc063583b5dcd93f24
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-15 01:22:45 -0300

    Update alter.c

--- alter.c
+++ alter.c
@@ -1,5 +1,3 @@
-int s,s1; // tamanhos dos vetores
-// 0=False- 1=True
 void alterna (int x){
     int n[2x];
     for (int i=0; i<=2x; i++){
@@ -10,6 +8,4 @@ void alterna (int x){
         n[i]=(i)*(-1);
         i++;
         }
-   
-    
 }
