77f312fdc90a8af6d433f0b66b8ddad6f88cda59
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-15 01:08:56 -0300

    Create maioresque.c

---
+++ maioresque.c
@@ -0,0 +1,13 @@
+int s; // tamanho do vetor
+// 0=False- 1=True
+void maioresQue (int x, int v[], int m[]){
+    int c;
+    for (int i=0; i<s; i++){
+        if(v[i]>x){
+            m[c]=v[i]
+            c++;
+            printf("%d", m[c]);
+        }
+    }
+    
+}
